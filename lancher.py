456c55f6696ea100aa62098e657f2a5c083728ef
RasmusDue <rasduek@gmail.com>
2020-10-02 21:30:53 +0200

    map random

--- lancher.py
+++ lancher.py
@@ -1,10 +1,12 @@
 import pygame
+import random
 from object import tanks
+from object import ball
 
 #setup pygame
 pygame.init()
-display_width = 800
-display_height = 600
+display_width = 1920
+display_height = 1080
 display = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN)
 pygame.display.set_caption('Tanks Game - Brought To You By Oliver & Rasmus')
 
@@ -18,17 +20,35 @@ blue = (0, 0, 255)
 red = (200, 0 , 0)
 p1 = [400,400]
 p2 = [600,200]
+pball = [display_width/2-100,display_height/2-50]
 controls1 = ["K_LEFT", "K_RIGHT", "K_UP", "K_DOWN"]
 
 #images
+map1 = pygame.image.load('background1(1).png')
+map2 = pygame.image.load('background 2.png')
+map = []
 tank1 = pygame.image.load('blaa_tank.png')
-
-#pygame.transform.scale(tank1, (45, 45))
 tank2 = pygame.image.load('rod_tank.png')
+ball1 = pygame.image.load('bold.png')
 
 #create tank´s
 c1 = tanks(blue, p1, tank1)
 c2 = tanks(red, p2, tank2)
+
+#random map
+random_map = random.randint(0,1)
+print(random_map)
+if random_map == 0:
+    map = map1
+elif random_map == 1:
+    map = map2
+
+
+blocksGroup = pygame.sprite.Group()
+
+#create ball
+b = ball(pball, ball1)
+
 
 #Main game loop
 while not done:
@@ -41,13 +61,13 @@ while not done:
     if keys[pygame.K_LEFT] and c1.position[0]>0:
         c1.position[0] -= c1.speed
         c1.angle = 90
-    if keys[pygame.K_RIGHT] and c1.position[0]<700:
+    if keys[pygame.K_RIGHT] and c1.position[0]<display_width-c1.size:
         c1.position[0] += c1.speed
         c1.angle = -90
     if keys[pygame.K_UP] and c1.position[1]>0:
         c1.position[1] -= c1.speed
         c1.angle = 0
-    if keys[pygame.K_DOWN] and c1.position[1]<500:
+    if keys[pygame.K_DOWN] and c1.position[1]<display_height-c1.size:
         c1.position[1] += c1.speed
         c1.angle = 180
     if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
@@ -62,13 +82,13 @@ while not done:
     if keys[pygame.K_a] and c2.position[0]>0:
         c2.position[0] -= c2.speed
         c2.angle = 90
-    if keys[pygame.K_d] and c2.position[0]<700:
+    if keys[pygame.K_d] and c2.position[0]<display_width-c2.size:
         c2.position[0] += c2.speed
         c2.angle = -90
     if keys[pygame.K_w] and c2.position[1]>0:
         c2.position[1] -= c2.speed
         c2.angle = 0
-    if keys[pygame.K_s] and c2.position[1]<500:
+    if keys[pygame.K_s] and c2.position[1]<display_height-c2.size:
         c2.position[1] += c2.speed
         c2.angle = 180
     if keys[pygame.K_a] and keys[pygame.K_w]:
@@ -80,9 +100,13 @@ while not done:
     if keys[pygame.K_d] and keys[pygame.K_w]:
         c2.angle = -45
 
-    display.fill((0,0,0))
+
+    display.blit(map, (0, 0))
+    #display.fill((0,0,0))
     c1.update(display)
     c2.update(display)
+
+    b.update(display)
 
 
 
--- object.py
+++ object.py
@@ -6,13 +6,27 @@ pygame.init()
 
 class tanks():
     def __init__(self, color, position, tank):
-        self.size = 60
+        self.size = 100
         self.color = color
         self.position = position
-        self.speed = 4
+        self.speed = 8
         self.angle = 0
         self.tank_png = pygame.transform.scale(tank, (self.size, self.size))
 
     def update(self, frame):
         tank_rotate = pygame.transform.rotate(self.tank_png,self.angle)
         frame.blit(tank_rotate, (self.position[0], self.position[1]))
+
+    def collide(self, spriteGroup):
+        if pygame.sprite.spritecollide(self, spriteGroup, False):
+            self.speed = 0
+
+class ball():
+    def __init__(self, position, png):
+        self.speed = 8
+        self.size = 100
+        self.position = position
+        self.ball_png = pygame.transform.scale(png, (self.size, self.size))
+
+    def update(self, frame):
+        frame.blit(self.ball_png, (self.position[0], self.position[1]))
