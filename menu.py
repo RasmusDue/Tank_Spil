bd397ee321b82efd85e14b6cd9574183e5ccbf80
Oliver Holbech Sørensen <OliverHolbech@olivers-air-3.sde.local>
2020-10-06 09:44:17 +0200

    Menu

--- menu.py
+++ menu.py
@@ -1,27 +1,34 @@
 import pygame
-import pygame_menu
 
-pygame.init()
-surface = pygame.display.set_mode((600, 400))
+
+
+
 done = False
-
+pygame.init()
+clock = pygame.time.Clock()
+display_width = 1280
+display_height = 720
+display = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN)
+pygame.display.set_caption('Tanks Game - Brought To You By Oliver & Rasmus')
+myfont = pygame.font.SysFont("comicsansms", 68)
 
 def menu():
-    menu = pygame_menu.Menu(400, 600, 'Tank Problem', theme=pygame_menu.themes.THEME_DARK)
+    baggrund_menu = pygame.image.load('Menu_wallpaper.png')
+    baggrund_menu = pygame.transform.scale(baggrund_menu, (1280,720))
 
-    menu.add_text_input('Name :', default='John Doe')
-    menu.add_selector('Map Selection :', [('Soccer field', 1), ('Space field', 2)], onchange=set_map)
-    menu.add_button('Play', start_the_game)
-    menu.add_button('Quit', pygame_menu.events.EXIT)
-    menu.mainloop(surface)
+    display.blit(baggrund_menu,(0, 0))
+    mouse = pygame.mouse.get_pos()
+
+    if 1050+200 > mouse[0] > 200 and 300+75 > mouse[1] > 300:
+        pygame.draw.rect(display, (200,200,200), (1050,300,200,75))
+    #else:
+        #pygame.draw.rect(display, (250,250,250), (1050,300,200,75),0)
+    display.blit(myfont.render("PLAY", 100, (255,255,255)), (1050,300+20))
 
 
-def set_map(value, difficulty):
-    # Do the job here !
-    pass
 
-def start_the_game():
-    pass
+
+
 
 
 while not done:
@@ -30,3 +37,6 @@ while not done:
             done = True
 
     menu()
+
+    pygame.display.update()
+    clock.tick(60)
